3e39e3177844b5dc8760b1ac0e8e5458f0e7c487
fakestarbaby <fakestarbaby@gmail.com>
2012-08-24 20:46:24 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleFeedzirra::Application.config.secret_token = 'f474ddc7e014882656b55a2674aad995e9d6d6de4e18f357e00bf665e052b93ebfae4d8d727159e6211ce0d9a05ac44234065eda3dd4ea001ddec9063d06edfb'
